0d0a300880d2445da9ab0da99b46d58c63be3ca8
Kristiina Suominen <kristiina.suominen@skillio.ai>
2024-02-21 13:32:21 +0200

    Add user input validation to update

--- src/queries.py
+++ src/queries.py
@@ -57,6 +57,7 @@ def validate_user_input(starttime, endtime, lunchbreak, consultname, customernam
 
 # Add daily workhours for a consult
 
+
 def db_add_workhours(starttime, endtime, lunchbreak, consultname, customername):
     val = validate_user_input(starttime, endtime, lunchbreak, consultname, customername)
     if val != "everything ok":
@@ -85,12 +86,9 @@ def db_add_workhours(starttime, endtime, lunchbreak, consultname, customername):
     return {"error": "problems with connection"}
 
 
-
 # Delete row from workhours table
 def db_delete_workhours(id):
-    query = sql.SQL(
-        "DELETE FROM workhours WHERE id = %s"
-    )
+    query = sql.SQL("DELETE FROM workhours WHERE id = %s")
     con = connect()
     if con is not None:
         cursor = con.cursor()
@@ -99,13 +97,18 @@ def db_delete_workhours(id):
         cursor.close()
         con.close()
 
+
 # Update daily workhours for a consult
 def db_update_workhours(id, starttime, endtime, lunchbreak, consultname, customername):
+    val = validate_user_input(starttime, endtime, lunchbreak, consultname, customername)
+    if val != "everything ok":
+        return val
+
     query = sql.SQL(
-        ''' UPDATE workhours 
+        """ UPDATE workhours 
             SET starttime = %s, endtime = %s, lunchbreak = %s, consultname = %s, customername = %s 
             WHERE id = %s;
-        '''
+        """
     )
     con = connect()
     if con is not None:
@@ -116,19 +119,17 @@ def db_update_workhours(id, starttime, endtime, lunchbreak, consultname, custome
         con.commit()
         cursor.close()
         con.close()
+        return {"success": "updated workhours for: %s" % consultname}
+    return {"error": "problems with connection"}
 
 
 def db_get_workhours_by_consult(consultname):
-    query = sql.SQL(
-        "SELECT * FROM workhours WHERE consultname = %s"
-    )
+    query = sql.SQL("SELECT * FROM workhours WHERE consultname = %s")
     con = connect()
     if con is not None:
         cursor = con.cursor()
-        cursor.execute(query,(consultname,))
-        data=cursor.fetchall()
+        cursor.execute(query, (consultname,))
+        data = cursor.fetchall()
         cursor.close()
         con.close()
     return data
-
-
